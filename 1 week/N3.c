951c8f168a1bfefa885dfc7295252a3e1738c9a2
hoeyoon <hoeiyoon57@gmail.com>
2023-09-09 15:45:01 +0900

    세 번째 문제

--- 1 week/N3.c
+++ 1 week/N3.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 int main(){
-  int x, y, z;
+  int x[3];
   puts("3개의 정수를 입력하세요.");
-  printf("정수 1 : ");
-  scanf("%d", &x);
+  
+  for(int i = 0; i < 3; i++){
+    printf("정수 %d : ", i + 1);
+    scanf("%d", &x[i]);
+  }
 
-  printf("정수 2 : ");
-  scanf("%d", &y);
-
-  printf("정수 3 : ");
-  scanf("%d", &z);
-
-  printf("이 세 값의 계산 결과는 %d입니다.\n", x + y - z);
+  printf("이 세 값의 계산 결과는 %d입니다.\n", x[0] + x[1] - x[2]);
   return 0;
 }
