33c477a82cda1c6942fefd4c3c1232e78c1ade8f
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2023-09-12 12:36:08 +0900

    12

---
+++ Book/chapter 1/12.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int n1, n2, n3;
+  puts("3개의 정숫값을 입력하세요.");
+  printf("정수 1 : "); 
+  scanf("%d", &n1);
+
+  printf("정수 2 : ");
+  scanf("%d", &n2);
+
+  printf("정수 3 : ");
+  scanf("%d", &n3);
+
+  printf("세 값의 합은 %d입니다.\n", n1 + n2 + n3);
+  
+  return 0;
+}
