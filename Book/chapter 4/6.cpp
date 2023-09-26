d4821a62189d6e24d2ba646c7fabe06dd878b07b
hoeyoon <hoeiyoon57@gmail.com>
2023-09-26 11:40:38 +0900

    6

---
+++ Book/chapter 4/6.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int i, no;
+
+  printf("양의 정수를 입력하세요: ");
+  scanf("%d", &no);
+
+  if(no >= 1){
+    int i = 2;
+    while(i <= no){
+      printf("%d ", i);
+      i += 2;   
+    }
+    printf("\n");
+  }
+  return 0;
+}
