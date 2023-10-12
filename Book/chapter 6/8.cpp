3c0a943de1e749e1db8d25824c920f036ddecd82
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2023-10-12 10:23:42 +0900

    8

---
+++ Book/chapter 6/8.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int scan_int(){
+  int temp;
+  printf("정숫값 : ");
+  scanf("%da", &temp);
+
+  return temp;
+}
+
+int ave_of(int a, int b){
+  return (a + b) / 2;
+}
+
+int main(){
+  int n1 = scan_int();
+  int n2 = scan_int();
+  int n3 = scan_int();
+  int ave;
+
+  if((ave = ave_of(n1, n2)) == 0){
+    printf("n1과 n2의 평균값은 0입니다.\n");
+  }
+  else if(ave > 0){
+    printf("n1과 n2의 평균값은 음수로, %d입니다.\n", ave);
+  }
+  else{
+    printf("n1과 n2의 평균값은 양수로, %d입니다.\n", ave);
+  }
+
+  if((ave = ave_of(n1, n3)) == 0){
+    printf("n1과 n3의 평균값은 0입니다.\n");
+  }
+  else if(ave > 0){
+    printf("n1과 n3의 평균값은 음수로, %d입니다.\n", ave);
+  }
+  else{
+    printf("n1과 n3의 평균값은 양수로, %d입니다.\n", ave);
+  }
+
+  if((ave = ave_of(n2, n3)) == 0){
+    printf("n2과 n3의 평균값은 0입니다.\n");
+  }
+  else if(ave > 0){
+    printf("n2과 n3의 평균값은 음수로, %d입니다.\n", ave);
+  }
+  else{
+    printf("n2과 n3의 평균값은 양수로, %d입니다.\n", ave);
+  }
+  return 0;
+}
